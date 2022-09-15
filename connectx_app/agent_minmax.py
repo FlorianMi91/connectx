b9df0c8524d29c7e98fa170655d2bfa0472ec6c8
FlorianMi91 <michonneau.f@gmail.com>
2022-09-15 16:32:52 +0200

    agent theo

--- connectx_app/agent_minmax.py
+++ connectx_app/agent_minmax.py
@@ -1,76 +1,31 @@
-
-
 import numpy as np
 import random
 
-
-  
-class saved_hash():
-  def __init__(self,flag,value):
-    self.flag = flag
-    self.value = value
-
-
-def ordering_moves(valid_moves,position,mask):
-  oppo = position ^ mask
-  final_valid_moves = []
-  for col in valid_moves:
-    child_position, child_mask = make_move(position, mask, col)
-    nb_of_threes = connected_three(child_position, child_position ^ child_mask, False)
-    final_valid_moves.append([nb_of_threes,col])
-  
-  final_valid_moves.sort(reverse=True)
-  return [move[1] for move in final_valid_moves]
-
-
-
-
-
-def negmax_pruning(position, mask, depth, color, alpha, beta):
-  alpha_original = alpha
+def minimax(position, mask, depth, maximizingPlayer, alpha, beta):
   ordered = [3,2,4,1,5,0,6]
   is_terminal = bool(check_four_aligned(position) + check_four_aligned(position ^ mask))
   valid_moves = [i for i in ordered if not (mask >> (i*7+5)) & 1]
-  hash_position = hash((position,mask,position ^ mask))
-
-  if hash_position in seen:
-    stored = seen[hash_position]
-    if stored.flag == 'EXACT':
-      return seen[hash_position].value
-    elif stored.flag == 'LOWERB':
-      alpha = max(alpha, stored.value)
-    elif stored.flag == 'UpperBB':
-      beta = min(beta, stored.value)
-    
-    if alpha >= beta:
-      return stored.value
-    
   if depth == 0 or is_terminal:
-      return color * get_euristic(position, mask, depth) 
-  value = -np.Inf
-  for col in valid_moves:
-    if color == -1:
-      child_position, child_mask = make_move(position ^ mask, mask, col)
-      child_position = child_position ^ child_mask
-    else:
-      child_position, child_mask = make_move(position, mask, col)
-
-    value = max(value, -negmax_pruning(child_position, child_mask, depth-1, -color, -beta, -alpha)) 
-    alpha = max(alpha,value)
-    if alpha >= beta:# or alpha > 90000000:
-      break
-    
-
-  if value <= alpha_original:
-    flag = 'UPPERB'
-  elif value >= beta:
-    flag = 'LowerB'
+      return get_euristic(position, mask)
+  if maximizingPlayer:
+      value = -np.Inf
+      for col in valid_moves:
+          child_position, child_mask = make_move(position, mask, col)
+          value = max(value, minimax(child_position, child_mask, depth-1, False, alpha, beta))
+          alpha = max(alpha, value)
+          if value >= beta:
+            break
+      return value
   else:
-    flag = 'EXACT'
-
-  seen[hash_position] = saved_hash(flag,value)
-  return value
-
+      value = np.Inf
+      for col in valid_moves:
+          child_position, child_mask = make_move(position ^ mask, mask, col)
+          child_position = child_position ^ child_mask
+          value = min(value, minimax(child_position, child_mask, depth-1, True, alpha, beta))
+          beta = min(beta, value)
+          if value <= alpha:
+            break
+      return value
 
 def make_move(position, mask, col):
   new_oppo = position ^ mask
@@ -78,22 +33,15 @@ def make_move(position, mask, col):
   new_position = new_oppo ^ new_mask
   return new_position, new_mask
 
-
-
-def score_move_negmax(grid,mask, col, depth):
-  
+def score_move(grid,mask, col, depth):
   new_position, new_mask = make_move(grid, mask, col)
-  score = -negmax_pruning(new_position, new_mask, depth, -1, -np.Inf, np.Inf)
+  score = minimax(new_position, new_mask, depth, False, -np.Inf, np.Inf )
   #score = get_euristic(new_position, new_mask)
   return score
 
 
-
-
 #check the alignement of 3 dangerous consecutive peeble in one direction
 def Check_3_alligned(position, oppo, code):
-  #import random
-  #import numpy as np
   #horizontal check : code = nb line + 1
   #vertical check : code = 1
   #diagonal / check : code = nb line + 2
@@ -129,25 +77,57 @@ def Check_3_alligned(position, oppo, code):
   return final_check
 
 #check the alignement of 3 dangerous consecutive peeble in all directions for all player
-def connected_three(position,oppo, check_oppo = True):   
+def connected_three(position,oppo):   
   check = Check_3_alligned(position, oppo, 7)
   check = check | Check_3_alligned(position, oppo, 1)
   check = check | Check_3_alligned(position, oppo, 6)
   check = check | Check_3_alligned(position, oppo, 8)
   # print(make_board(check)) 
   # print("check connected 3")
-  if check_oppo:
-    check_oppo = Check_3_alligned(oppo, position, 7)
-    check_oppo = check_oppo | Check_3_alligned(oppo, position, 1)
-    check_oppo = check_oppo | Check_3_alligned(oppo, position, 6)
-    check_oppo = check_oppo | Check_3_alligned(oppo, position, 8)
+
+  check_oppo = Check_3_alligned(oppo, position, 7)
+  check_oppo = check_oppo | Check_3_alligned(oppo, position, 1)
+  check_oppo = check_oppo | Check_3_alligned(oppo, position, 6)
+  check_oppo = check_oppo | Check_3_alligned(oppo, position, 8)
   # print(make_board(check)) 
   # print("check connected 4")
-    return [count_set_bits(check),count_set_bits(check_oppo)]
 
+  return [count_set_bits(check),count_set_bits(check_oppo)]
 
-  return count_set_bits(check)
+#check the alignement of 3 dangerous consecutive peeble in one direction
+def Check_2_alligned(position, oppo, code):
+  #horizontal check : code = nb line + 1
+  #vertical check : code = 1
+  #diagonal / check : code = nb line + 2
+  #diagonal \ check : code = nb line
+  number_of_two = [0, 0]
+  check1 = position & (position >> code)
+  check1 = check1 >> code
+  comparator = check1 & oppo
+  check1 = check1 - comparator
+  count_set_bits(check1)
+  
+  check2 = position & (position << code)
+  check2 = check2 << code
+  comparator = check2 & oppo
+  check2 = check2 - comparator
+  return check2 | check1
 
+#check the alignement of 2 dangerous consecutive peeble in all directions for all player
+def connected_two(position,oppo):
+  
+
+  check = Check_2_alligned(position, oppo, 7)
+  check = check | Check_2_alligned(position, oppo, 1)
+  check = check | Check_2_alligned(position, oppo, 6)
+  check = check | Check_2_alligned(position, oppo, 8)
+
+  check_oppo = Check_2_alligned(oppo, position, 7)
+  check_oppo = check_oppo | Check_2_alligned(oppo, position, 1)
+  check_oppo = check_oppo | Check_2_alligned(oppo, position, 6)
+  check_oppo = check_oppo | Check_2_alligned(oppo, position, 8)
+
+  return [count_set_bits(check),count_set_bits(check_oppo)]
 
 def check_four_aligned(position):
   # Horizontal check
@@ -169,11 +149,12 @@ def check_four_aligned(position):
   # Nothing found
   return 0
 
-def get_euristic(position, mask, depth):
+def get_euristic(position, mask):
   oppo = position ^ mask
   number_of_3 = connected_three(position,oppo)
+  number_of_2 = connected_two(position,oppo)
   number_of_4 = [check_four_aligned(position),check_four_aligned(oppo)]
-  euristic = number_of_3[0] * 100 - number_of_3[1] * 100 + (number_of_4[0] * 100000000 * (depth +1)) - (number_of_4[1] * 100000000 * (depth +1))
+  euristic = number_of_2[0] * 1 - number_of_2[1] * 1 + number_of_3[0] * 100 - number_of_3[1] * 100 + number_of_4[0] * 1000000 - number_of_4[1] * 100000
   #return number_of_2
   return euristic
 
@@ -226,24 +207,9 @@ def get_position_mask_bitmap(board, player):
           position += ['0', '1'][board[i, j] == player]
   return int(position, 2), int(mask, 2)
   #return position, mask
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-def my_agent_binary_negmax(obs, config, test = False, depth= 8, player = 1): #20 = 10.58
-  
-  
-  
-  
+
+def my_agent_binary_negmax(obs, config, test = False, depth= 6, player = 1): 
   ordered = [3,2,4,1,5,0,6]
-  global seen
-  seen = {}
   if test:
     columns = 7
     rows = 6
@@ -263,7 +229,7 @@ def my_agent_binary_negmax(obs, config, test = False, depth= 8, player = 1): #20
   valid_moves = [i for i in ordered if not (mask >> (i*7+5)) & 1]
 
   # Use the heuristic to assign a score to each possible board in the next step
-  scores = dict(zip(valid_moves, [score_move_negmax(grid, mask, col, depth) for col in valid_moves]))
+  scores = dict(zip(valid_moves, [score_move(grid, mask, col, depth) for col in valid_moves]))
 
   # Get a list of columns (moves) that maximize the heuristic
   max_cols = [key for key in scores.keys() if scores[key] == max(scores.values())]
@@ -274,7 +240,7 @@ def my_agent_binary_negmax(obs, config, test = False, depth= 8, player = 1): #20
   if test :
     return scores
   return random.choice(max_cols)
-
+  
 def check_winner(board):
   grid = np.asarray(board).reshape(6, 7)
   position, mask = get_position_mask_bitmap(grid,1)
---
+++ connectx_app/agent_ppo2.py
@@ -0,0 +1,21 @@
+from stable_baselines import PPO2
+import numpy as np
+import random
+
+
+
+
+model = PPO2.load('connectx_app/agents_weights/ppo2weight')
+
+#print(model, ' :model')
+
+def agent_theo(obs, config):
+    # Use the best model to select a column
+    col, _ = model.predict(np.array(obs['board']).reshape(6,7,1))
+    # Check if selected column is valid
+    is_valid = (obs['board'][int(col)] == 0)
+    # If not valid, select random move. 
+    if is_valid:
+        return int(col)
+    else:
+        return random.choice([col for col in range(config.columns) if obs.board[int(col)] == 0])
